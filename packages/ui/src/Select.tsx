0fceaf48d410f766fc573e081ecc2f0db3dc3c4d
Kunal Walmiki <kunalwalmiki15@gmail.com>
2024-06-26 01:04:17 +0530

    Updated package.json in ui

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -6,7 +6,7 @@ const Select = ({options, onSelect} : {
     options : {
         key : string,
         value : string,
-    }[]
+    }[],
     }
 ) => {
   return (
@@ -14,7 +14,7 @@ const Select = ({options, onSelect} : {
         onSelect(e.target.value)
     }}
     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-        {options.map(options => <option value={options.key}>{options.value}</option>)}
+        {options.map((options, index) => <option key={index} value={options.key}>{options.value}</option>)}
     </select>
   )
 }
